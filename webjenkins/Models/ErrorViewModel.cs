1ab952369524aeee675c8efe84595631dfce3c73
cjr <2849584264@qq.com>
2021-03-01 15:23:34 +0800

    新建

---
+++ webjenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webjenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
